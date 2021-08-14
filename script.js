77660fdbd6dff018a20026c517edd5fda1da9861
Brigitte Powers <reyhn701@gmail.com>
2021-08-13 22:26:16 -0500

    Added criteria check and mass password array

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var pwc = {
   letters: "abcdefghijklmnopqrstuvwxyz",
   numeric: "013456789",
   special: "!@#$%&*",
-  length: "8",
+  length: 8
 };
 
 // Write password to the #password input
@@ -22,19 +22,67 @@ function writePassword() {
 
 // Add event listener to buttons
 generateBtn.addEventListener("click", writePassword);
-// TODO: criteriaBtn.addEventListener("click", );
+criteriaBtn.addEventListener("click", showCriteria);
 
+// user clicks button, presented w series of prompts for password criteria
+//show or hide criteria card
+function showCriteria() {
+  var criteriaDisplay = document.querySelector(".criteria-box");
+  if (criteriaDisplay.classList.contains("hidden")) {
+    criteriaDisplay.classList.remove("hidden");
+    criteriaDisplay.classList.add("shown");
+  } else {
+    criteriaDisplay.classList.remove("shown");
+    criteriaDisplay.classList.add("hidden");
+  }
+}
 
-// When user clicks button, they are presented with a series of prompts for password criteria
+//criteria validity check
+//length criteria requirements
+var lengthInput = document.querySelector("#length");
+lengthInput.addEventListener("change", updateLength);
+function updateLength() {
+  if (lengthInput.value <8 ) {
+    //alert ("Please insert a number between 8 to 128");
+    lengthInput.value = 8
+  } else if (lengthInput.value >128 ) {
+    //alert ("Please insert a number between 8 to 128");
+    lengthInput.value = 128;
+  }
+  pwc.length = lengthInput.value;
+  console.log(pwc.length);
+}
 
+function generatePassword() {
+  // reset selected
+  pwc.selected = "";
 
+  // obtain user criteria
+  var lowercaseInput = document.querySelector("#lowercase")
+  var uppercaseInput = document.querySelector("#uppercase")
+  var numbersInput = document.querySelector("#numbers")
+  var specialInput = document.querySelector("#special")
+
+  // adds user criteria to selected array
+  if (lowercaseInput.checked == true) {
+    pwc.selected = pwc.selected.concat(pwc.letters);
+  }
+  if (uppercaseInput.checked == true) {
+    pwc.selected = pwc.selected.concat(pwc.letters.toUpperCase());
+  }
+  if (numbersInput.checked == true) {
+    pwc.selected = pwc.selected.concat(pwc.numeric);
+  }
+  if (specialInput.checked == true) {
+    pwc.selected = pwc.selected.concat(pwc.special);
+  }
+  console.log(pwc.selected)
+}
+
+// at least one character type should be selected
 
 // Randomly generated password meeting creiteria
-// When user clicks button they are presented with a series of prompts for password critera
 
-// User selects crtieria to include in the password
-
-// Input should be validated and at least one character type should be selected
-// When all prompts are answered, generator a password matching selected critera
+// When all prompts are answered, generate password matching selected critera
 // Displayed as an alert or written to the page
 
