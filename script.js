efec3b44338a9df4c7d59b06c01cb8e12b15616d
Brigitte Powers <reyhn701@gmail.com>
2021-08-13 23:48:39 -0500

    Refactored code

--- script.js
+++ script.js
@@ -1,30 +1,58 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var criteriaBtn = document.querySelector("#criteria");
+var lengthInput = document.querySelector("#length");
 
 // Password criteria
 var pwc = {
-  selected: "",
   letters: "abcdefghijklmnopqrstuvwxyz",
   numeric: "013456789",
   special: "!@#$%&*",
   length: 8
 };
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// randomize password and return to writePassword
+function generatePassword() {
+  var randomPassword = "";
+  var selected = "";
 
-  passwordText.value = password;
+  // obtain user criteria
+  var lowercaseInput = document.querySelector("#lowercase")
+  var uppercaseInput = document.querySelector("#uppercase")
+  var numbersInput = document.querySelector("#numbers")
+  var specialInput = document.querySelector("#special")
 
+  // adds user criteria to selected array
+  if (lowercaseInput.checked == true) {
+    selected = selected.concat(pwc.letters);
+  }
+  if (uppercaseInput.checked == true) {
+    selected = selected.concat(pwc.letters.toUpperCase());
+  }
+  if (numbersInput.checked == true) {
+    selected = selected.concat(pwc.numeric);
+  }
+  if (specialInput.checked == true) {
+    selected = selected.concat(pwc.special);
+  }
+
+  // generate password matching length
+  for (var i=0; i < pwc.length; i++) {
+    var randChar = randomGen(selected);
+    randomPassword = randomPassword.concat(randChar);
+  }
+  return randomPassword;
+} 
+
+//Random number gen
+function randomGen(optionList) {
+  //random integer from 1 - amt of options available
+  var randIdx = Math.floor(Math.random() * optionList.length);
+  var random = optionList[randIdx];
+  return random;
 }
 
-// Add event listener to buttons
-generateBtn.addEventListener("click", writePassword);
-criteriaBtn.addEventListener("click", showCriteria);
-
-// user clicks button, presented w series of prompts for password criteria
+// user presented with series of prompts for password criteria
 //show or hide criteria card
 function showCriteria() {
   var criteriaDisplay = document.querySelector(".criteria-box");
@@ -37,52 +65,27 @@ function showCriteria() {
   }
 }
 
-//criteria validity check
 //length criteria requirements
-var lengthInput = document.querySelector("#length");
-lengthInput.addEventListener("change", updateLength);
 function updateLength() {
   if (lengthInput.value <8 ) {
-    //alert ("Please insert a number between 8 to 128");
     lengthInput.value = 8
   } else if (lengthInput.value >128 ) {
-    //alert ("Please insert a number between 8 to 128");
     lengthInput.value = 128;
   }
   pwc.length = lengthInput.value;
   console.log(pwc.length);
 }
 
-function generatePassword() {
-  // reset selected
-  pwc.selected = "";
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-  // obtain user criteria
-  var lowercaseInput = document.querySelector("#lowercase")
-  var uppercaseInput = document.querySelector("#uppercase")
-  var numbersInput = document.querySelector("#numbers")
-  var specialInput = document.querySelector("#special")
-
-  // adds user criteria to selected array
-  if (lowercaseInput.checked == true) {
-    pwc.selected = pwc.selected.concat(pwc.letters);
-  }
-  if (uppercaseInput.checked == true) {
-    pwc.selected = pwc.selected.concat(pwc.letters.toUpperCase());
-  }
-  if (numbersInput.checked == true) {
-    pwc.selected = pwc.selected.concat(pwc.numeric);
-  }
-  if (specialInput.checked == true) {
-    pwc.selected = pwc.selected.concat(pwc.special);
-  }
-  console.log(pwc.selected)
+  passwordText.value = password;
 }
 
-// at least one character type should be selected
-
-// Randomly generated password meeting creiteria
-
-// When all prompts are answered, generate password matching selected critera
-// Displayed as an alert or written to the page
+// Add event listener to buttons
+generateBtn.addEventListener("click", writePassword);
+criteriaBtn.addEventListener("click", showCriteria);
+lengthInput.addEventListener("change", updateLength);
 
